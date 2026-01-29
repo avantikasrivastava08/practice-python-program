c4acaa91a7df84b8e4814ecff0d26842096051b3
avantikasrivastava08 <sage4ya@gmail.com>
2026-01-29 22:21:05 +0530

    Add files via upload

---
+++ pg3.py
@@ -0,0 +1,6 @@
+a= int(input(" enter number 1 "))
+b= int(input("enter number 2 "))
+if a<b:
+    print(" the greatest number amongst the two is: ",b)
+else:
+    print("the greatest number amongst the two is: ",a)
